103c3d0b7a739f6a47d85fe75b3b4934b9bc3d3b
Lukáš Spurný <spurny.lukas@post.cz>
2024-03-21 13:57:35 +0100

    impl SwifterSwift and SwiftDate

--- Package.swift
+++ Package.swift
@@ -14,12 +14,14 @@ let package = Package(
             targets: ["PekoFramework"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tadelv/Wormholy", .upToNextMajor(from: "1.6.5"))
+        .package(url: "https://github.com/tadelv/Wormholy", .upToNextMajor(from: "1.6.5")),
+        .package(url: "https://github.com/SwifterSwift/SwifterSwift", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/malcommac/SwiftDate", .upToNextMajor(from: "7.0.0"))
     ],
     targets: [
         .target(
             name: "PekoFramework",
-            dependencies: ["Wormholy"]
+            dependencies: ["Wormholy", "SwifterSwift", "SwiftDate"]
         )
     ]
 )
