caaafef1c84352e7e8f545e58781d1dc8b6d73bd
Lukáš Spurný <spurny.lukas@post.cz>
2024-03-26 09:38:03 +0100

    update encodable extension

--- Sources/PekoFramework/Extensions/Encodable.swift
+++ Sources/PekoFramework/Extensions/Encodable.swift
@@ -8,6 +8,25 @@
 import Foundation
 
 public extension Encodable {
+    var convertToString: String? {
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(self)
+            return String(data: jsonData, encoding: .utf8)
+        } catch {
+            return nil
+        }
+    }
+    
+    var convertToData: Data? {
+        let jsonEncoder = JSONEncoder()
+        do {
+            return try jsonEncoder.encode(self)
+        } catch {
+            return nil
+        }
+    }
+    
     var dict: [String: Any]? {
         guard let data = try? JSONEncoder().encode(self) else {
             return nil
